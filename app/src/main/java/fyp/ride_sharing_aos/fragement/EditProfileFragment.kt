3f4b0a50106b2dc32f16379e69fc0e64b3ccfc41
Henry Sze <henryszex42s@gmail.com>
2018-02-26 20:14:20 +0800

    Some change on gradle

--- app/src/main/java/fyp/ride_sharing_aos/fragement/EditProfileFragment.kt
+++ app/src/main/java/fyp/ride_sharing_aos/fragement/EditProfileFragment.kt
@@ -38,8 +38,7 @@ class EditProfileFragment : Fragment() {
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
         val view = inflater!!.inflate(R.layout.fragment_edit_profile, null)
